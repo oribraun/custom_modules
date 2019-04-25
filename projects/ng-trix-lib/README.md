52523e09c331f3e348e52693d22d8c524f38982a
orib <oribrau@gmail.com>
2019-04-25 20:05:25 +0300

    add name input to lib

--- projects/ng-trix-lib/README.md
+++ projects/ng-trix-lib/README.md
@@ -1,4 +1,4 @@
-# NgTrixLib
+# NgTrixLib `Trix 1.1.1`
 
 This library was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.2.0.
 
--- projects/ng-trix-lib/src/lib/ng-trix-lib.component.ts
+++ projects/ng-trix-lib/src/lib/ng-trix-lib.component.ts
@@ -1,11 +1,10 @@
 import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
 import {ControlValueAccessor} from "@angular/forms";
 
-import "../../node_modules/trix/dist/trix.js";
 @Component({
     selector: 'trix-editor',
     template: '',
-    styleUrls: ['../../node_modules/trix/dist/trix.css'],
+    styleUrls: [],
     encapsulation: ViewEncapsulation.None
 })
 export class NgTrixLibComponent implements ControlValueAccessor, OnInit {
